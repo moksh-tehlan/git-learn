68272572af0204732f0c9bbaaaa69ea5fca2ab84
Moksh Tehlan <tehlanmoksh@gmail.com>
2023-07-29 09:00:08 +0530

    bug 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        bug1();
+    }
+    static void bug1(){
+        System.out.println("bug 1");
+    }
+}
