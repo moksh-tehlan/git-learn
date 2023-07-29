6c5e571905bcb367c9aac74ddf576618707bc0db
Moksh Tehlan <tehlanmoksh@gmail.com>
2023-07-29 09:01:59 +0530

    bug 2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         bug1();
+        bug2();
     }
     static void bug1(){
         System.out.println("bug 1");
+    }
+
+    static void bug2(){
+        System.out.println("bug 3 resolved");
     }
 }
